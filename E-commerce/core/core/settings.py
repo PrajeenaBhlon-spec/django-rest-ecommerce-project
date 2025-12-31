a2c3e3e08c4063057ea7bf7e2a4ecdf098917850
PrajeenaBhlon <prajeenabhlon@gmail.com>
2025-12-31 13:00:38 +0545

    Update settings.py

--- E-commerce/core/core/settings.py
+++ E-commerce/core/core/settings.py
@@ -84,9 +84,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "prajeenabhlon@gmail.com"
-EMAIL_HOST_PASSWORD = "msqo ycwe guoq qstj"
-DEFAULT_FROM_EMAIL = "prajeenabhlon@gmail.com"
+EMAIL_HOST_USER = 'tourstories36@gmail.com'
+EMAIL_HOST_PASSWORD = 'xsjy oxkw aikw kpkx'
+DEFAULT_FROM_EMAIL = 'tourstories36@gmail.com'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
