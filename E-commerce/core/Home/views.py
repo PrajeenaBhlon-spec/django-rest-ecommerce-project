705b58936abdb9180fb498e3f24ccf41d78f302d
prajeenabhlon <prajeenabhlon@gmail.com>
2026-02-24 14:42:41 +0545

    feat: working on chekout button

--- E-commerce/core/Home/views.py
+++ E-commerce/core/Home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render 
 from django.views.decorators.cache import cache_control
-
+from product.models import Product
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def home_page(request):
@@ -11,3 +11,7 @@ def home_page(request):
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def user_home_page(request):
   return render(request , "Home/userHome.html")
+
+def productViewBeforeLogin(request):
+  products = Product.objects.all()
+  return render(request , 'Home/userProductView.html' , {"products": products})
